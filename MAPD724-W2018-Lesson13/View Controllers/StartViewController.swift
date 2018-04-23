3a69364daecf70c194d31efb76e0f5d2dd1d66fd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-04-23 19:54:32 -0400

    Added animations on StartViewController

--- MAPD724-W2018-Lesson13/View Controllers/StartViewController.swift
+++ MAPD724-W2018-Lesson13/View Controllers/StartViewController.swift
@@ -5,24 +5,50 @@ class StartViewController: UIViewController {
 
     @IBOutlet weak var StartScreenLabel: UILabel!
     
+    @IBOutlet weak var NextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        StartScreenLabel.alpha = 0
+        NextButton.isHidden = true
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         
+        AnimateLabelIn()
+        AnimateButtonIn()
+        MoveButtonOver()
     }
     
     @IBAction func NextButton(_ sender: UIButton) {
         performSegue(withIdentifier: SegueManager.SecondView, sender: sender)
+    }
+    
+    func AnimateLabelIn() {
+        UIView.animate(withDuration: 1, delay: 0.25,
+                       options: [.curveEaseInOut], animations: {
+                        self.StartScreenLabel.alpha = 1
+                        self.StartScreenLabel.center.y -= 50
+        }, completion: nil)
+    }
+    
+    func AnimateButtonIn() {
+        UIView.transition(with: self.NextButton, duration: 2,
+                          options: [.transitionFlipFromTop], animations: {
+                          self.NextButton.isHidden = false
+        }, completion: nil)
+    }
+    
+    func MoveButtonOver() {
+        UIView.animate(withDuration: 2, delay: 0.25,
+                       options: [.curveEaseIn], animations: {
+                        self.NextButton.center.x += 125
+        }, completion: nil)
     }
     
     
