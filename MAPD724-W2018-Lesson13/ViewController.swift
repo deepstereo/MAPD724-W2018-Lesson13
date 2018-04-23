47315679fcd24d210e57684ba0e38ad400de2f57
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-04-23 18:39:03 -0400

    Initial Commit

---
+++ MAPD724-W2018-Lesson13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD724-W2018-Lesson13
+//
+//  Created by Tom Tsiliopoulos on 2018-04-23.
+//  Copyright © 2018 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
